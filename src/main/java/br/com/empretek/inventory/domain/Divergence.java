4397c162ccbb13d0b0fafbf53560905c3dde1875
Jacob Cabral <jacob-cabral@live.com>
2017-11-20 21:19:10 -0200

    Inclusão do método que retorna os registros associados - getRecords() - e do código identificador da divergência.

--- src/main/java/br/com/empretek/inventory/domain/Divergence.java
+++ src/main/java/br/com/empretek/inventory/domain/Divergence.java
@@ -1,20 +1,38 @@
 package br.com.empretek.inventory.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Divergence {
 
+  private static final long UNDEFINED_CODE = -1;
+
+  private long code;
   private List<Record> records;
 
   public Divergence() {
     super();
+    code = UNDEFINED_CODE;
     records = new LinkedList<Record>();
   }
 
+  public Divergence(Record... records) {
+    this();
+
+    if (records == null || Arrays.asList(records).isEmpty())
+      throw new IllegalArgumentException("Os registros não podem ser nulos ou vazios.");
+
+    Arrays.asList(records).forEach(record -> add(record));
+  }
+
   public boolean add(Record record) {
-    if (!validateEquals(record))
+    if (record != null && !validateEquals(record))
       throw new IllegalArgumentException("Registo inválido. O código do registro informado difere dos demais.");
+
+    if (code == UNDEFINED_CODE && record != null)
+      code = record.getCode();
 
     return records.add(record);
   }
@@ -24,6 +42,10 @@ public class Divergence {
       throw new IllegalArgumentException("Registro inválido. O código do registro informado difere dos demais.");
 
     return records.remove(record);
+  }
+  
+  public List<Record> getRecords() {
+    return Collections.unmodifiableList(records);
   }
 
   private boolean validateEquals(Record record) {
@@ -35,17 +57,45 @@ public class Divergence {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (int) (code ^ (code >>> 32));
+    result = prime * result + ((records == null) ? 0 : records.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Divergence other = (Divergence) obj;
+    if (code != other.code)
+      return false;
+    if (records == null) {
+      if (other.records != null)
+        return false;
+    } else if (!records.equals(other.records))
+      return false;
+    return true;
+  }
+
+  @Override
   public String toString() {
     StringBuffer buffer = new StringBuffer();
     buffer.append("Divergência [\n");
-    
+
     records.forEach(registro -> {
       buffer.append(registro.toString());
       buffer.append("\n");
     });
 
     buffer.append("]");
-        
+
     return buffer.toString();
   }
 }
